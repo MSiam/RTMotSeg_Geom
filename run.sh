8206b23e1047194dee1b9a479bbd16ae7aff55f2
mennatullah siam <mennatul@ualberta.ca>
2018-02-11 05:57:30 -0700

    2stream mobilenet

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 ###################################### MobileNet ##################################################
 #python3 main.py --load_config=fcn8s_mobilenet_train.yaml train Train FCN8sMobileNet
 #python3 main.py --load_config=fcn8s_mobilenet_test.yaml test Train FCN8sMobileNet
+python3 main.py --load_config=fcn8s_2stream_mobilenet_train.yaml train Train2Stream FCN8s2StreamMobileNet
 
 ###################################### ShuffleNet ##################################################
 #1- FCN8s ShuffleNet Train Coarse+Fine
