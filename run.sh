6e677ee77f9fa2c2f9373bf7be30b1be6386807c
mennatullah siam <mennatul@ualberta.ca>
2018-02-23 12:28:41 -0700

    early fusion mobilenet trial

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ###################################### MobileNet ##################################################
 #python3 main.py --load_config=fcn8s_mobilenet_train.yaml train Train FCN8sMobileNet
 #python3 main.py --load_config=fcn8s_mobilenet_test.yaml test Train FCN8sMobileNet
-#python3 main.py --load_config=fcn8s_2stream_mobilenet_train.yaml train Train2Stream FCN8s2StreamMobileNet
+python3 main.py --load_config=fcn8s_2stream_mobilenet_train.yaml train Train2Stream FCN8s2StreamMobileNet
 
 ###################################### ShuffleNet ##################################################
 #1- FCN8s ShuffleNet Train Coarse+Fine
@@ -13,6 +13,6 @@
 #2- FCN8s ShuffleNet Test
 #python3 main.py --load_config=fcn8s_shufflenet_test.yaml test Train FCN8sShuffleNet
 #python3 main.py --load_config=fcn8s_2stream_shufflenet_test.yaml test Train2Stream FCN8s2StreamShuffleNet
-python3 main.py --load_config=fcn8s_2stream_shufflenet_test.yaml test Train2Stream FCN8s2StreamShuffleNet3
+#python3 main.py --load_config=fcn8s_2stream_shufflenet_test.yaml test Train2Stream FCN8s2StreamShuffleNet3
 #python3 main.py --load_config=fcn8s_2stream_shufflenet_test.yaml inference Train2Stream FCN8s2StreamShuffleNet
 #python3 main.py --load_config=fcn8s_shufflenet_test.yaml inference Train FCN8sShuffleNet
